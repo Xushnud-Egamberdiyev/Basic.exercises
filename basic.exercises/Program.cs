3c00a7614150defc1308dfbb21210e4517fe4c73
Xushnud Egamberdiyev <140577111+Xushnud-Egamberdiyev@users.noreply.github.com>
2024-04-29 11:22:03 +0500

    Add project files.

---
+++ basic.exercises/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace basic.exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
