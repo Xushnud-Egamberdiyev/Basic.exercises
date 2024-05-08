b0a452e32fa1847e1c3f38225bab14d57aca4ee2
Xushnud Egamberdiyev <140577111+Xushnud-Egamberdiyev@users.noreply.github.com>
2024-05-08 23:31:15 +0500

    Add files via upload
    blazor chat created

---
+++ SignaIRWebApp/SignaIRWebApp/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignaIRWebApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageToAll(string user, string message) =>
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+        public async Task SendMessageToCaller(string user, string message) =>
+            await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+
+        public async Task SendMessageToCaller(string user, string message, string connectionId) =>
+            await Clients.Group("SignalR Users").SendAsync("ReceiveMessage", user, message);
+
+        public async Task SendMessageToGroup(string user, string message) =>
+           await Clients.Group("SignalR Users").SendAsync("ReceiveMessage", user, message);
+
+    }
+}
