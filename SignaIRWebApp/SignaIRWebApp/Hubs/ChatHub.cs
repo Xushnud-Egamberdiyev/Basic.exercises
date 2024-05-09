fc80612796e6572e5cc188c0c0dbf0c2b17e1869
Xushnud Egamberdiyev <140577111+Xushnud-Egamberdiyev@users.noreply.github.com>
2024-05-09 09:19:09 +0500

    blazor chat write

---
+++ SignaIRWebApp/SignaIRWebApp/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignaIRWebApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageToAll(string user, string message) =>
+            await Clients.All.SendAsync("ReceiveMessage", Context.ConnectionId, message);
+
+        public async Task SendMessageToCaller(string user, string message) =>
+            await Clients.Caller.SendAsync("ReceiveMessage", Context.ConnectionId, message);
+
+        public async Task SendMessageToUser(string user, string message, string connectionId) =>
+            await Clients.Client(connectionId).SendAsync("ReceiveMessage", Context.ConnectionId, message);
+
+        public async Task SendMessageToGroup(string user, string message) =>
+           await Clients.Group("SignalR Users").SendAsync("ReceiveMessage", Context.ConnectionId, message);
+
+    }
+}
