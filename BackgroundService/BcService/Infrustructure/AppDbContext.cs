17c743491d334165bb4c62e2e492b1d1443a3df9
Xushnud Egamberdiyev <140577111+Xushnud-Egamberdiyev@users.noreply.github.com>
2024-05-23 12:34:32 +0500

    add

---
+++ BackgroundService/BcService/Infrustructure/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BcService.Infrustructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+    }
+}
