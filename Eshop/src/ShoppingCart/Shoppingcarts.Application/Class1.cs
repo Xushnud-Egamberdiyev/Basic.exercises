78d799f13f693635f54a1010ffe23f1ad2aab65f
Xushnud Egamberdiyev <140577111+Xushnud-Egamberdiyev@users.noreply.github.com>
2024-08-07 10:51:14 +0500

    update file

--- Eshop/src/ShoppingCart/Shoppingcarts.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Shoppingcarts.Application
-{
-    public class Class1
-    {
-
-    }
-}
--- Eshop/src/ShoppingCart/Shoppingcarts.Infrastructure/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Shoppingcarts.Infrastructure
-{
-    public class Class1
-    {
-
-    }
-}
